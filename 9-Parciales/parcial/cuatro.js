0e2e696d47ec1da6d56800adac54e860269988f8
alumno <alumno>
2018-02-15 11:31:39 -0300

    parcial 4 completo con dos formas de escribir if-else if/else - if/else if/else-if /else

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,35 @@
 function Mostrar()
 {
+var num1, num2, resultado;
+num1 = prompt("Ingrese numero 1");
+num2 = prompt("Ingrese numero 2");
 
+    num1=parseInt(num1);
+    num2=parseInt(num2);
+
+    if (num1 == num2) {
+        resultado = num1 * num2;
+        document.write(resultado);
+    } else {
+        if (num1 > num2) {
+            resultado= num1 - num2;
+            document.write(resultado);
+        } else {
+            resultado= num1 + num2;
+            document.write(resultado);
+        }
+    }
+
+
+//    if (num1 == num2) {                       Otra forma de escribir el codigo if-else-if-else
+//        resultado = num1 * num2;
+//        document.write(resultado);
+//    } else if (num1 > num2) {
+//            resultado= num1 - num2;
+//            document.write(resultado);
+//        } else {
+//            resultado= num1 + num2;
+//            document.write(resultado);
+//        }
+    
 }
