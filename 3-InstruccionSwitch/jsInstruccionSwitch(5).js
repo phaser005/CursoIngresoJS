8104edc016a59399ec2dca896cea3385a08c6fed
alumno <alumno>
2018-02-16 10:10:30 -0300

    ejercicios 5 y 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -6,9 +6,16 @@ var laHora = document.getElementById('hora').value;
 //alert (laHora);
 	
 switch (laHora) {
-    case """":
-        
+    case "7":
+    case "8":
+    case "9":
+    case "10":
+    case "11":
+        alert("Es de mañana.");
         break;
+    
+    default:
+        alert("No es de mañana.");
 }	
 
 
