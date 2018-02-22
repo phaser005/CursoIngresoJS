445a52d17789f1ce2aa7ec0aac56529095e6a678
Elias <36265028+phaser005@users.noreply.github.com>
2018-02-22 10:08:48 -0300

    Ejercicio 4
    4

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,13 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	numero = parseInt(numero);
 
+while (numero<0 || numero>9) {
+	alert("ingrese nuevamente.");
+	numero = prompt("ingrese un número entre 0 y 10.");
+}
+
+document.getElementById("Numero").value = numero;
 
 }//FIN DE LA FUNCIÓN
